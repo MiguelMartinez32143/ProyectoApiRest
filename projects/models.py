065b3d8b51bf6f8875190b613fb9cf177a79c781
samuel <samuelcr11@outlook.com>
2026-03-18 09:32:33 -0500

    first commit

---
+++ projects/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
