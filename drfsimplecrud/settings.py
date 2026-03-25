84af7664daa6fd63cc518e7686458978ed106ef5
samuel <samuelcr11@outlook.com>
2026-03-25 08:38:07 -0500

    fix: add .onrender.com to ALLOWED_HOSTS for production

--- drfsimplecrud/settings.py
+++ drfsimplecrud/settings.py
@@ -35,6 +35,9 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+if not DEBUG:
+    ALLOWED_HOSTS.append(".onrender.com")
 # Application definition
 
 INSTALLED_APPS = [
